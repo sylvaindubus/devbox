1c76606d282e019be3d42139b0a3055f9ade954f
Sylvain DUBUS <sdubus@kds.com>
2021-11-17 18:38:35 +0100

    Add Hyper.js config

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ set -euo pipefail
 
 # Add zsh configuration
 echo 'Creating a new symbolic link for zsh'
-rm ~/.zshrc
+rm -f ~/.zshrc
 ln -s "${PWD}/config/.zshrc" "${HOME}/.zshrc"
 
 # Add vim configuration
 echo 'Creating a new symbolic link for vim'
-rm ~/.vimrc
+rm -f ~/.vimrc
 ln -s "${PWD}/config/.vimrc" "${HOME}/.vimrc"
+
+# Add Hyper.js configuration
+echo 'Creating a new symbolic link for Hyper.js'
+rm -f ~/.hyper.js
+ln -s "${PWD}/config/.hyper.js" "${HOME}/.hyper.js"
 
 echo "Done :)"
 exit 0
